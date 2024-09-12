01d3b59ae009868cb6267a10047767a295d0f670
faisalhatila <hatilas1994@gmail.com>
2024-09-12 09:07:51 +0500

    logo added on nac

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const Logo = () => {
-  return (
-    <div className="flex items-center">
-      <h1 className="text-primary text-4xl relative after:cotent-[''] after:w-[9px] after:h-[9px] after:absolute after:bg-white after:bottom-[7px] after:ml-[5px] after:rounded-[50%]">
-        i labs
-      </h1>
-    </div>
-  );
+  return <img src="/assets/images/company-logo.png" />;
 };
 
 export default Logo;
